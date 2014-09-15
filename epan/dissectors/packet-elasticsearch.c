417192e9d1e5c783bf28c61de00535c1e264f995
Ryan Doyle <ryan@doylenet.net>
2014-09-15 13:48:31 +1000

    Typo

--- epan/dissectors/packet-elasticsearch.c
+++ epan/dissectors/packet-elasticsearch.c
@@ -21,7 +21,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 #include "config.h"
-version
 #include <epan/packet.h>
 
 #define ELASTICSEARCH_DISCOVERY_PORT 54328
